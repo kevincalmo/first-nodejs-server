475b2cffe21fbf74b197acdcf84c44669146ce51
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-05-21 15:25:10 -0400

    mise en pla ce des routes avec des fichiers externes et d'une page 404 not found

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const express = require('express');
+
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
 const bodyParser = require('body-parser');
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/product-page', (req, res, next) => {
-    res.send("<body><form method='POST' action='/product'><input type='text' name='title' > <button type='submit'>Add title</button>'</form></body>");
-});
+app.use(adminRoute);
+app.use(shopRoute);
 
-//le middleware sera en poste sur /product
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>HELLO FROM EXPRESS !</h1>');
+//Faire une page 404
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found Bro !!!</h1>');
 });
 
 app.listen(9090);
