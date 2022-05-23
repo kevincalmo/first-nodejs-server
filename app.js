5256dc9a6faa758e995930f713f90c957a35ab57
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-05-22 21:25:36 -0400

    Ajout du moteur de template ejs pour le rendu html

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const express = require('express');
 
-const adminRoute = require('./routes/admin');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoute = require('./routes/shop');
+const userData = require('./routes/user');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const app = express()
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'public')));//donne accès au fichier du dossier public 'public'
+app.use(express.static(path.join(__dirname, 'public')));//donne accès au fichier du dossier public 'public'
 
-app.use('/admin',adminRoute); // '/admin' est un filtre, toute les routes de adminRoute seront accessibles avec le prefixe /admin/nom_route_adminRoute
+app.use('/admin', adminData.routes); // '/admin' est un filtre, toute les routes de adminRoute seront accessibles avec le prefixe /admin/nom_route_adminRoute
 app.use(shopRoute);
+app.use('/user',userData.routes);
 
 //Faire une page 404
 app.use('*', (req, res, next) => {
     console.log('hello');
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).render('404', { pageTitle: 'Page non trouvé :(' });
 });
 
-app.listen(9090);
+app.listen(3000);
