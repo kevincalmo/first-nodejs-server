349a46c2aefc1aef8e56eebe3c371138bd5bd065
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-05-22 21:26:55 -0400

    exportations des produits crés

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,19 @@ const router = express.Router();
 const path = require('path');
 const rootDir = require('../utils/path');
 
+const products = [];
+
 router.use('/product-page', (req, res, next) => {
-res.sendFile(path.join(rootDir,'views','product.html'));
+res.render('product');
 });
 
 //le middleware sera en poste sur /product
 router.post('/product', (req, res, next) => {
     console.log(req.body);
+    products.push({title: req.body.title});
+    console.log(products);
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;// j'xporte le tableau pour qu'il soit accessible partout
