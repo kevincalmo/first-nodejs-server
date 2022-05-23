ec0b3a0c7d5db4cd2fc904df35c058f7faf9d6ed
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-05-22 21:27:25 -0400

    Affichage des utilisateurs et des produits dans la vue de la page d'accueil

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const adminData = require('../routes/admin');
+const userData = require('../routes/user');
 
 const path = require('path');
 const rootDir = require('../utils/path');
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir,'views','shop.html'));
+    console.log(adminData.products);
+    res.render('shop', {
+        products: adminData.products,
+        pageTitle: 'Home Page',
+        users: userData.users
+    });
 });
 
 module.exports = router;
