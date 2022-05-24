90774c276a89fa7eb228b4b19de5c7c39734de3c
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-05-23 20:49:30 -0400

    Ajout des valeurs pour utilisé la vue

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,8 +12,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => console.log(product));
-  res.redirect('/');
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: 'products/' + product.id
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
