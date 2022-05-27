7e3c8deb9ed525428beb44c03711a6549d1108d1
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-05-27 19:53:01 -0400

    Ajout de nouveaux controlleurs

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/add-product', {
     pageTitle: 'Add Product',
-    path: '/admin/add-product',
+    path: '/admin/edit-product',
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true
@@ -18,6 +18,21 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  res.render('admin/edit-product', {
+    pageTitle: 'Edit Product',
+    path: '/admin/edit-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+    editing: editMode
+  });
 };
 
 exports.getProducts = (req, res, next) => {
