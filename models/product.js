ec7b661f122839b805e447cb40041406e5a4270f
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-05-27 22:49:48 -0400

    Appel au model du panier et ajout de la fonction statique delete pour supprimer un produit

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -47,6 +49,19 @@ module.exports = class Product {
     });
 
   }
+
+  static delete(id) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(p => p.id === id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+          Cart.deleteProduct(id);
+        }
+      });
+    });
+  }
+
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
