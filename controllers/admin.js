087acc6d7b3a7e62863ff955d09300db64276bc8
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-05-27 21:41:45 -0400

    Ajout de la possibilité d'édité un produit

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,13 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/edit-product',
     formsCSS: true,
     productCSS: true,
-    activeAddProduct: true
+    activeAddProduct: true,
+    editing: false
   });
 };
 
@@ -15,7 +16,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -25,14 +26,35 @@ exports.getEditProduct = (req, res, next) => {
   if (!editMode) {
     return res.redirect('/');
   }
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: '/admin/edit-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
-    editing: editMode
+  const proId = req.params.productId;
+  Product.findById(proId, product => {
+    if (!product) return res.redirect('/');
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      formsCSS: true,
+      productCSS: true,
+      activeAddProduct: true,
+      editing: editMode,
+      product: product
+    });
   });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const uptatedTitle = req.body.title;
+  const uptatedImageUrl = req.body.imageUrl;
+  const uptatedPrice = req.body.price;
+  const uptatedDescription = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    uptatedTitle,
+    uptatedImageUrl,
+    uptatedDescription,
+    uptatedPrice
+  );
+  updatedProduct.save();
 };
 
 exports.getProducts = (req, res, next) => {
