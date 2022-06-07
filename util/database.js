e8b524ae2abb2369df61984682e0c283d2dbd688
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-06-07 19:12:58 -0400

    Connexion à la db avec l'orm sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,7 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'admin',
-    database: 'node_complete',
-    password: 'admin'
+const sequelize = new Sequelize('node_complete', 'admin', 'admin', {
+    dialect: 'mysql', host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
