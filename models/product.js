829596b8bf9ae036e37239679583e70df885b034
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-06-17 19:32:51 -0400

    Update

--- models/product.js
+++ models/product.js
@@ -2,12 +2,13 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = id;
+    this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
   }
 
   save() {
@@ -17,7 +18,7 @@ class Product {
       // Update the product
       dbOp = db
         .collection('products')
-        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+        .updateOne({ _id: this._id }, { $set: this });
     } else {
       dbOp = db.collection('products').insertOne(this);
     }
@@ -58,6 +59,15 @@ class Product {
       .catch(err => {
         console.log(err);
       });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .deleteOne({ _id: mongodb.ObjectId(prodId) })
+      .then(result => { console.log(result); })
+      .catch(err => console.log(err));
   }
 }
 
