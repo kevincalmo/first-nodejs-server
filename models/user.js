925b295a5c17fc30435f2fb970c620fb94f6ca4a
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-06-19 08:17:06 -0400

    Ajout des methods

--- models/user.js
+++ models/user.js
@@ -26,4 +26,38 @@ const userSchema = new Schema({
   }
 });
 
+/* Mongoose permet de créer ses propre fonction pour nos usage spécifique
+avec Schema.methods */
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+    return cp.productId.toString() === product._id.toString();
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+  if (cartProductIndex >= 0) {
+    //si le produit existe deja dans le panier
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    //si le produit n'existe pas encore dans le panier
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems
+  };
+  this.cart = updatedCart;
+  return this.save();
+}
+
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart = updatedCartItems;
+  return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema);
