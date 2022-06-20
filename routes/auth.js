b084973d9685ba975053d0e00ef81c6c42c8cbe2
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-06-20 17:57:29 -0400

    Ajout d'une route postlogin et postlogout

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 router.get('/login', authController.getLogin);
+router.post('/login', authController.postLogin);
+router.post('/logout', authController.postLogout);
 
 module.exports = router;
